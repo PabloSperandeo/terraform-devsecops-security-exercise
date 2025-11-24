f3177966f2708b308acde7d36c9678f9fdaf4315
amaurybsouza <amaurybsouza@gmail.com>
2025-11-24 16:04:03 -0300

    teste

---
+++ terraform-code-example.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.0.0-beta2"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "sa-east-1"
+  shared_credentials_files = ["/home/amaurybs/.aws/credentials"]
+  profile                  = "default"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
